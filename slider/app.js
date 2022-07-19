3ae1af8f09d25fcca5244a38da294a7f7939aef8
swvalerian <valera.panafidin@teamgetter.com>
2022-07-19 11:12:46 +0900

    feat: Add project_3 - Slider

---
+++ slider/app.js
@@ -0,0 +1,42 @@
+const downBtn = document.querySelector('.down-button');
+const upBtn = document.querySelector('.up-button');
+
+const sideBar = document.querySelector('.sidebar');
+
+const mainSlide = document.querySelector('.main-slide');
+
+const container = document.querySelector('.container');
+
+const slidesCount = mainSlide.querySelectorAll('div').length;
+
+sideBar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+let activeSlideIndex = 0;
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+});
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+});
+
+const heightWindow = container.clientHeight;
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+    mainSlide.style.transform = `translateY(-${heightWindow * activeSlideIndex}px)`;
+    
+    sideBar.style.transform = `translateY(${heightWindow * activeSlideIndex}px)`;
+}
+
